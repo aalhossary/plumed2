2f857a6639f49413483961dcf2074e6e71300f2e
Amr ALHOSSARY <amr_alhossary@hotmail.com>
2026-03-18 12:11:20 -0400

    use new kenref CORE path structure

--- src/kenref/runFiles/runPlumed_double.sh
+++ src/kenref/runFiles/runPlumed_double.sh
@@ -1,4 +1,14 @@
-source /smithlab/opt/gromacs-dev/2025/2025.3/debug/AVX_512/bin/GMXRC
+#!/bin/bash
+# Updated to use GROMACS-4-PLUMED installation
+source /smithlab/opt/gromacs-4-plumed/2025/2025.3/debug/AVX_512/bin/GMXRC
 source /home/amr/git/plumed2/sourceme.sh
+
+# Verify PLUMED is loaded
+if [ -z "$PLUMED_KERNEL" ]; then
+    echo "ERROR: PLUMED_KERNEL not set. Please check PLUMED installation."
+    exit 1
+fi
+echo "PLUMED_KERNEL=$PLUMED_KERNEL"
+
 #When you run multi simulations, you need to have the file in a relative folder
 mpirun -n 2 gmx_mpi mdrun -s topol.tpr -multidir repl_01 repl_02 -nsteps 50 -plumed ../../runFiles/test_double.dat # -deffnm $fnm &>> $outname
